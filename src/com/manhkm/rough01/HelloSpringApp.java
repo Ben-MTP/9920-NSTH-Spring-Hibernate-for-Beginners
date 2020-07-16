a6520d7d174074e43c4f41e6bd58b812c2e089cb
BEN_MTP <minhmanh.office.uet@gmail.com>
2020-07-16 09:40:55 +0700

    6-Spring Inversion of Control - Write Some Code

---
+++ src/com/manhkm/rough01/HelloSpringApp.java
@@ -0,0 +1,24 @@
+package com.manhkm.rough01;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		//load the spring configuration file:
+		/*
+		 * Class: ClassPathXmlApplicationContext -> what is this?
+		 */
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retreive bean from spring container:
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		//call methods on the bean:
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close the context:
+		context.close();
+	}
+
+}
